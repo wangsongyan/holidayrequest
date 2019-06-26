425b2e9611cfcacb431c5d5348e457b7069f0be4
wangsongyan <1104237534@qq.com>
2019-06-26 10:07:59 +0800

    deploy

--- src/main/java/org/flowable/HolidayRequest.java
+++ src/main/java/org/flowable/HolidayRequest.java
@@ -2,7 +2,10 @@ package org.flowable;
 
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
 
 public class HolidayRequest {
 
@@ -13,6 +16,17 @@ public class HolidayRequest {
 				.setJdbcPassword("")
 				.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
 		ProcessEngine processEngine = cfg.buildProcessEngine();
+		
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		Deployment deployment = repositoryService.createDeployment()
+				.addClasspathResource("holiday-request.bpmn20.xml")
+				.deploy();
+		
+		ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+				.deploymentId(deployment.getId())
+				.singleResult();
+		System.out.println("Found process definition: " + processDefinition.getName());
+		
 	}
 
 }
